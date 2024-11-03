6b2e66558d54f05f6030a5b8c3d1a9aa50fada1a
roshiya152 <muthupranav523@gmail.com>
2024-11-03 14:24:06 +0530

    Update Todo.js

--- client/src/component/Todo.js
+++ client/src/component/Todo.js
@@ -18,7 +18,7 @@ const Todo = () => {
 
     try {
       const response = await axios.post(
-        "https://todolistreactapp-server.onrender.com/todos",
+        "https://todolist-reactapplication.onrender.com",
         { todo: newTodo },
         {
           headers: {
@@ -45,7 +45,7 @@ const Todo = () => {
 
     try {
       const responseget = await axios.get(
-        "https://todolistreactapp-server.onrender.com/todos",
+        "https://todolist-reactapplication.onrender.com",
         {
           headers: {
             Authorization: `Bearer ${token}`, // Send token in Authorization header
@@ -71,7 +71,7 @@ const Todo = () => {
 
     try {
       // Build the URL based on the filter
-      let url = "https://todolistreactapp-server.onrender.com/todos";
+      let url = "https://todolist-reactapplication.onrender.com";
       if (filter === "completed") {
         url += "?completed=true";
       } else if (filter === "uncompleted") {
@@ -120,7 +120,7 @@ const Todo = () => {
 
     try {
       await axios.delete(
-        `https://todolistreactapp-server.onrender.com/todos/${todoId}`,
+        `https://todolist-reactapplication.onrender.com/todos/${todoId}`,
         {
           headers: {
             Authorization: `Bearer ${token}`, // Send the token in the Authorization header
@@ -142,7 +142,7 @@ const Todo = () => {
     try {
       // Assuming editingTodo contains the user's ID and the todo ID
       await axios.put(
-        `https://todolistreactapp-server.onrender.com/todos/${editingTodo._id}`, // Adjusted URL to include userId
+        `https://todolist-reactapplication.onrender.com/todos/${editingTodo._id}`, // Adjusted URL to include userId
         {
           todo: updatedTodo,
           completed: updatedStatus, // Ensure the key matches what your backend expects
